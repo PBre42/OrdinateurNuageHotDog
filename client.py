878c417ac5dc4696fb49cccedcc4f012af0141de
PBre42 <49883776+PBre42@users.noreply.github.com>
2021-10-08 16:09:16 +0200

    Update client.py

--- client.py
+++ client.py
@@ -1,1 +1,11 @@
+import boto3
+#connect to server and send request
+sqs = boto3.resource('sqs')
+queue = sqs.create_queue(QueueName='requestQueue',Attributes={'DelaySeconds' : '5'})
+
+
+print("Enter values (space separated):")
+input = input()
+queueOut = sqs.get_queue_by_name(QueueName='requestQueue')
+queueOut.send_message(MessageBody=input)
 
