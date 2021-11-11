efc77999f3935d636e0f71673f5dea7f91f0b1f2
PBre42 <49883776+PBre42@users.noreply.github.com>
2021-11-11 14:40:51 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,16 +1,20 @@
 import boto3
-
+import random
 #launch the server and wait for requests
 ec2 = boto3.client('ec2')
 sqs = boto3.resource('sqs')
-s3 = boto3.client('s3')
-bucketImages = s3.Bucket('imageshotdogs')
-bucketJson = s3.Bucket('jsonhotdogs')
+s3 = boto3.resource('s3')
+bucketname = 'photoshotdogs'
+bucket = s3.Bucket(bucketname)
 
+urls = []
+for obj in bucket.objects.all():
+    urls.append("https://%s.s3.amazonaws.com/%s" % (bucketname,obj.key))
+
+clienturls = random.sample(urls,4)
 
 #queue = sqs.create_queue(QueueName='responseQueue', Attributes={'Min','Max','Mean','Median'})
-inQueue = sqs.get_queue_by_name(QueueName='requestQueue')
+#inQueue = sqs.get_queue_by_name(QueueName='requestQueue')
 
-
-print(inQueue.attributes.get('Message'))
+#print(inQueue.attributes.get('Message'))
 
