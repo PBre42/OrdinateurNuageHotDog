e1e7cb4037ad8104c3caf8b9348e8b2dfbcaf210
PBre42 <49883776+PBre42@users.noreply.github.com>
2021-11-11 16:08:35 +0100

    Update client.py

--- client.py
+++ client.py
@@ -2,38 +2,3 @@ import boto3
 #connect to server and send request
 sqs = boto3.client('sqs')
 
-queueOut = sqs.create_queue(
-    QueueName='requestQueue',
-    Attributes={
-        "DelaySeconds" : "0",
-        "VisibilityTimeout" : "60"}
-    )
-
-qURL = sqs.get_queue_url(
-    QueueName='requestQueue',
-)
-
-#queueIn = sqs.get_queue_by_name(QueueName='response')
-
-#condition pour attendre un message (boucle)
-while 1:
-    print("Enter values (space separated):")
-    inputs = input().split(' ')
-    #vérifier inputs
-    
-
-    #remplir la queue
-    sqs.send_message(
-        QueueUrl=qURL["QueueUrl"],
-        MessageBody=str(inputs)
-    )  
-    
-    #attendre la réponse
-    reponse = sqs.receive_message(
-        QueueUrl=qURL["QueueUrl"]
-    )
-    print(reponse['Body'])
-
-    #parser la queue 
-    
-    #afficher valeurs
