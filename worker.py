d3f3d3b718a5fcfdde979b3c6801e8465ec340e5
PBre42 <49883776+PBre42@users.noreply.github.com>
2021-11-11 16:08:22 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,27 +1,41 @@
 import boto3
 import random
+import json
+
 #launch the server and wait for requests
 ec2 = boto3.client('ec2')
 sqs = boto3.client('sqs')
 s3 = boto3.resource('s3')
 bucketname = 'photoshotdogs'
-bucket = s3.Bucket(bucketname)
+bucketname2 = 'hotdogsdata'
 
-urls = []
-for obj in bucket.objects.all():
-    urls.append("https://%s.s3.amazonaws.com/%s" % (bucketname,obj.key))
+def sendImages():
+    urls = []
+    #List pictures and parse their urls
+    for obj in s3.Bucket(bucketname).objects.all():
+        urls.append("https://%s.s3.amazonaws.com/%s" % (bucketname,obj.key))
+    #Get random pictures        
+    clienturls = random.sample(urls,4)
+    #Send 4 urls
+    response = sqs.send_message(
+            QueueUrl="photosHotDogs",
+            MessageBody=' '.join(clienturls)
+        )  
 
-clienturls = random.sample(urls,4)
+def storeDataInJson(data):
+    #data parse ou a parser
+    s3.Object(bucketname2,'votes.json').put(Body=bytes(json.dumps(data).encode('UTF-8')))
 
-print(clienturls)
-#queue = sqs.create_queue(QueueName='photos', Attributes={'Url'})
-response = sqs.send_message(
-        QueueUrl="photosHotDogs",
-        MessageBody=' '.join(clienturls)
-    )  
+def uploadImages(picture):
+    #Create a key for new picture
+    imgCount = 0
+    for obj in s3.Bucket(bucketname).objects.all():
+        imgCount += 1
+    #Create key
+    extension = picture.split('.',2)
+    key = "%s." + extension % (imgCount+1) 
+    
+    #Upload to S3
+    s3.upload_file(picture,bucketname,key)
 
-
-#inQueue = sqs.get_queue_by_name(QueueName='requestQueue')
-
-#print(inQueue.attributes.get('Message'))
 
