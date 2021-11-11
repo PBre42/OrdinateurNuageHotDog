f8071a604bae5c0c6023aa1aadc51bd0e0f6119e
PBre42 <49883776+PBre42@users.noreply.github.com>
2021-11-11 13:02:15 +0100

    Update worker.py

--- worker.py
+++ worker.py
@@ -3,6 +3,11 @@ import boto3
 #launch the server and wait for requests
 ec2 = boto3.client('ec2')
 sqs = boto3.resource('sqs')
+s3 = boto3.client('s3')
+bucketImages = s3.Bucket('imageshotdogs')
+bucketJson = s3.Bucket('jsonhotdogs')
+
+
 #queue = sqs.create_queue(QueueName='responseQueue', Attributes={'Min','Max','Mean','Median'})
 inQueue = sqs.get_queue_by_name(QueueName='requestQueue')
 
