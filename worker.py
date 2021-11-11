116db813b545a3df37f0e06bc6f705cdc06d6169
PBre42 <49883776+PBre42@users.noreply.github.com>
2021-11-11 19:49:43 +0100

    img selection

--- worker.py
+++ worker.py
@@ -8,22 +8,24 @@ from werkzeug.utils import secure_filename
 
 ACCESS_KEY_ID = ''
 ACCESS_SECRET_KEY = ''
-bucketname = 'mybucket12344433443'
-bucketname2 = 'mybucket12344433443'
+bucketname2 = 'hotdogsdata'
+bucketname = 'photoshotdogs'
 
 #launch the server and wait for
-ec2 = boto3.client('ec2')
 sqs = boto3.client('sqs')
 s3 = boto3.resource('s3')
 
-
 UPLOAD_FOLDER = "uploads"
-
 app=Flask(__name__)
 
 @app.route("/")
 def home():
-    return render_template("index.html")
+    urls = []
+     #List pictures and parse their urls
+    for obj in s3.Bucket(bucketname).objects.all():
+        #Get random pictures
+        urls.append("https://%s.s3.amazonaws.com/%s" % (bucketname,obj.key))
+    return render_template("index.html",list_to_send=random.sample(urls,4))
 
 
 @app.route("/upload")
@@ -41,19 +43,16 @@ def upload():
 if __name__ == "__main__":
     app.run(debug=True)
 
-# def sendImages():
-#     urls = []
-#     #List pictures and parse their urls
-#     for obj in s3.Bucket(bucketname).objects.all():
-#         urls.append("https://%s.s3.amazonaws.com/%s" % (bucketname,obj.key))
-#     #Get random pictures
-#     clienturls = random.sample(urls,4)
-#     #Send 4 urls
-#     response = sqs.send_message(
-#             QueueUrl="photosHotDogs",
-#             MessageBody=' '.join(clienturls)
-#         )
-#
+
+def sendImages():
+     urls = []
+     #List pictures and parse their urls
+     for obj in s3.Bucket(bucketname).objects.all():
+         urls.append("https://%s.s3.amazonaws.com/%s" % (bucketname,obj.key))
+     #Get random pictures
+     return random.sample(urls,4)
+
+
 # def storeDataInJson(data):
 #     #data parse ou a parser
 #     s3.Object(bucketname2,'votes.json').put(Body=bytes(json.dumps(data).encode('UTF-8')))
